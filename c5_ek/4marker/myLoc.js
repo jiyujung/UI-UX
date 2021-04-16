c6d412fe1ce929dc57180c8d3ad1c2c77ca08630
jiyujung <s2019w39@e-mirim.hs.kr>
2021-04-16 09:50:45 +0900

    map-marker

--- c5_ek/4marker/myLoc.js
+++ c5_ek/4marker/myLoc.js
@@ -71,33 +71,35 @@ function showMap(coords) {
 	var mapDiv = document.getElementById("map");
 	map = new google.maps.Map(mapDiv, mapOptions);
 
-// 사용자 마커를 추가
-//추가2
-
-
-
+	// 사용자 마커를 추가
+	//추가2
+	var title = "당신의 위치";
+	var content = " 당신은 여기에 있습니다: " +
+		coords.latitude + ", " + coords.longitude;
+	addMarker(map, googleLatAndLong, title, content);
 }
 
 function addMarker(map, latlong, title, content) {
-//추가1
+	//추가1
+	var markerOptions = {
+		position: latlong,
+		map: map,
+		title: title,
+		clickable: true
+	};
+	var marker = new google.maps.Marker(markerOptions);
 
+	var infoWindowOptions = {
+		content: content,
+		position: latlong
+	};
 
+	var infoWindow = new google.maps.InfoWindow(infoWindowOptions);
 
-
-
-
-
-
-	
-
-
-
-
-
-
-
+	google.maps.event.addListener(marker, 'click', function () {
+		infoWindow.open(map);
+	});
 }
-
 
 function displayError(error) {
 	var errorTypes = {
