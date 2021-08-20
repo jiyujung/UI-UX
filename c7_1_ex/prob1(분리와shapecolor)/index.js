07d69be37566a612a714846d1175664f97314b83
s2019w39 <s2019w39@e-mirim.hs.kr>
2021-08-20 10:14:02 +0900

    separate html,css,js

--- c7_1_ex/prob1(분리와shapecolor)/index.js
+++ c7_1_ex/prob1(분리와shapecolor)/index.js
@@ -10,9 +10,9 @@ function previewHandler() {
 
 	fillBackgroundColor(canvas, context);
 
-
-	
-	
+	var selectObj = document.getElementById("shape");
+	var index = selectObj.selectedIndex;
+	var shape = selectObj[index].value;
 
 	if (shape == "squares") {
 		for (var squares = 0; squares < 15; squares++) {
@@ -45,46 +45,52 @@ function fillBackgroundColor(canvas, context) {
 
 // Draws a square at a random location
 function drawSquare(canvas, context) {
+	var selectObj = document.getElementById("shapeColor");
+	var index = selectObj.selectedIndex;
+	var color = selectObj[index].value;
 
+	var w = Math.floor(Math.random() * 40);    
+	var x = Math.floor(Math.random() * canvas.width);
+	var y = Math.floor(Math.random() * canvas.height);
 
-
-
-
-
-
-
-
-
+	context.fillStyle = color;
+	context.fillRect(x, y, w, w+20);
 }
 
 function drawCircle(canvas, context) {
+	var selectObj = document.getElementById("shapeColor");
+	var index = selectObj.selectedIndex;
+	var color = selectObj[index].value;
+
+	var radius = Math.floor(Math.random() * 40);
+	var x = Math.floor(Math.random() * canvas.width);
+	var y = Math.floor(Math.random() * canvas.height);
+
+	context.beginPath();
+	context.arc(x, y, radius, 0, degreesToRadians(360), true);
 
 
-
-
-
-
-
-
-
-
-
-
-
+	context.fillStyle = color;
+	context.fill();
 }
 
 function drawLine(canvas, context) {
+	var selectObj = document.getElementById("shapeColor");
+	var index = selectObj.selectedIndex;
+	var color = selectObj[index].value;
 
+	var x = Math.floor(Math.random() * canvas.width);
+	var y = Math.floor(Math.random() * canvas.height);
+	var x2 = Math.floor(Math.random() * canvas.width);
+	var y2 = Math.floor(Math.random() * canvas.height);
 
+	context.beginPath();
+	context.moveTo(x, y);
+	context.lineTo(x2, y2);
 
-
-
-
-
-
-
-
-
+	context.lineWidth = 7;
+	context.strokeStyle = color;
+	context.stroke();
 }
 
 
